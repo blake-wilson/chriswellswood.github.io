ee9abc555deeee889f55695778fa5dfa783a41a8
Chris Wells Wood <Chris Wells Wood>
2017-01-03 00:09:46 +0000

    Rejgged the site layout to have a site subfolder.

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off
 REM Build index
-elm-make .\\src\\Index.elm --output=index.js
+elm-make .\\src\\Index.elm --output=.\\site\\index.js
 
 REM Build posts
 SET pcr="posts\\code\\rust"
 elm-make .\\src\\content\\%pcr%\\EmptyRustStructs.elm^
- --output=%pcr%\\empty-rust-structs.js
+ --output=.\\site\\%pcr%\\empty-rust-structs.js
--- src/Index.elm
+++ src/Index.elm
@@ -1,7 +1,7 @@
 module Index exposing (..)
 
-import Html
-import Html.Attributes
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
 import Templates
 import Types exposing (ContentMetaData)
@@ -9,25 +9,25 @@ import Types exposing (ContentMetaData)
 import EmptyRustStructs
 import Snippets exposing (allSnippets)
 
-main : Html.Html msg
+main : Html msg
 main = Templates.basicPage view
 
-view : Html.Html msg
-view = Html.div []
+view : Html msg
+view = div []
     [ aboutMe
-    , Html.hr [] []
+    , hr [] []
     , recentPosts
-    , Html.hr [] []
+    , hr [] []
     , recentSnippets
-    , Html.hr [] []
+    , hr [] []
     ]
 
 -- About Me Section
 
-aboutMe : Html.Html msg
-aboutMe = Html.div []
-    [ Html.h2 [] [ Html.text "About Me"]
-    , Html.p [] [ Html.text aboutMeText]
+aboutMe : Html msg
+aboutMe = div []
+    [ h2 [] [ text "About Me"]
+    , p [] [ text aboutMeText]
     ]
 
 aboutMeText : String
@@ -41,22 +41,22 @@ articles/posts as well as snippets from other sources.
 
 -- Generic content functionality
 
-recentContentList : List ContentMetaData -> Html.Html msg
-recentContentList posts = Html.ol [] (List.map contentDetailItem posts)
+recentContentList : List ContentMetaData -> Html msg
+recentContentList posts = ol [] (List.map contentDetailItem posts)
 
-contentDetailItem : ContentMetaData -> Html.Html msg
+contentDetailItem : ContentMetaData -> Html msg
 contentDetailItem metaData =
-    Html.li []
-        [ Html.div []
-            [ Html.a [ Html.Attributes.href metaData.url ] [ contentTitle metaData ]
-            , Html.p [] [ Html.text metaData.description ]
+    li []
+        [ div []
+            [ a [ href metaData.url ] [ contentTitle metaData ]
+            , p [] [ text metaData.description ]
             ]
         ]
 
-contentTitle : ContentMetaData -> Html.Html msg
-contentTitle metaData = Html.h4 []
-    [ Html.a [ Html.Attributes.href metaData.url ]
-        [ Html.text (metaData.title ++ " - " ++ metaData.category ++ "/" ++ metaData.subcategory) ]
+contentTitle : ContentMetaData -> Html msg
+contentTitle metaData = h4 []
+    [ a [ href metaData.url ]
+        [ text (metaData.title ++ " - " ++ metaData.category ++ "/" ++ metaData.subcategory) ]
     ]
 
 -- Posts
@@ -66,16 +66,16 @@ allPosts =
     [ EmptyRustStructs.metaData
     ]
 
-recentPosts : Html.Html msg
-recentPosts = Html.div []
-    [ Html.h2 [] [ Html.text "Recent Posts" ]
+recentPosts : Html msg
+recentPosts = div []
+    [ h2 [] [ text "Recent Posts" ]
     , recentContentList (List.reverse (List.sortBy .date allPosts))
     ]
 
 -- Snippets
 
-recentSnippets : Html.Html msg
-recentSnippets = Html.div []
-    [ Html.h2 [] [ Html.text "Recent Snippets" ]
+recentSnippets : Html msg
+recentSnippets = div []
+    [ h2 [] [ text "Recent Snippets" ]
     , recentContentList (List.reverse (List.sortBy .date allSnippets))
     ]
